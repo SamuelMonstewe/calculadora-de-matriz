ff1f99cebb0b5b15d3ef79c284137b00282be824
SamuelMonstewe <samuelelias563@gmail.com>
2023-03-30 08:26:35 -0300

    efetuando-a-multiplicação

--- multiplicarMatrizes.hpp
+++ multiplicarMatrizes.hpp
@@ -9,6 +9,7 @@ void obterMultiplicacaoDeMatrizes()
     int NumeroColunasMatriz1;
     int NumeroLinhasMatriz2;
     int NumeroColunasMatriz2;
+    int ResultadoMultiplicacaoDeDoisElementos = 0;
     double Elemento;
 
     cout << "Digite a ordem da primeira matriz: " << endl;
@@ -18,6 +19,7 @@ void obterMultiplicacaoDeMatrizes()
 
     double Matriz1[NumeroLinhasMatriz1][NumeroColunasMatriz1];
     double Matriz2[NumeroLinhasMatriz2][NumeroLinhasMatriz2];
+    double MatrizResultado[NumeroLinhasMatriz1][NumeroColunasMatriz2];
 
     if(NumeroColunasMatriz1 == NumeroLinhasMatriz2)
     {   cout << "preencha a primeira matriz: " << endl;
@@ -39,9 +41,37 @@ void obterMultiplicacaoDeMatrizes()
             {
                 cout << "digite o elemento A" << i << j << endl;
                 cin >> Elemento;
-                Matriz2[i][j] = Elemento;          
+                Matriz2[i][j] = Elemento; 
+                       
             }
         }
+        
+        for(int i = 0; i < NumeroLinhasMatriz1; i++)
+        {
+            for(int j = 0; j < NumeroColunasMatriz2; j++)
+            {
+                MatrizResultado[i][j] = 0;
+                for(int k = 0; k < NumeroColunasMatriz1; k++)
+                {
+                    MatrizResultado[i][j] += Matriz1[i][k] * Matriz2[k][j];
+                }
+            }
+        }
+
+        for(int i = 0; i < NumeroLinhasMatriz1; i++)
+        {
+            for(int j = 0; j < NumeroColunasMatriz2; j++)
+            {
+                cout << MatrizResultado[i][j] << "\t";
+            }
+            cout << "\n";
+        }
+    
     }
+    else 
+        cerr << "Nao foi possivel fazer a multiplicacao. Verifique se voce deu a ordem das matrizes corretamente <j = i>" << endl;
+
+    
+    
 }
 #endif
